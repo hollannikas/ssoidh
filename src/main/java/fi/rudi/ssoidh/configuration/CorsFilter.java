4cfc54f7abbe79993f3fc7fa1c04264fa04a540d
Hollannikas <hollannikas@users.noreply.github.com>
2016-04-03 21:18:42 +0300

    added http access control

---
+++ src/main/java/fi/rudi/ssoidh/configuration/CorsFilter.java
@@ -0,0 +1,40 @@
+package fi.rudi.ssoidh.configuration;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet filter to set cross-origin access control
+ *
+ * Created by rudi on 03/04/16.
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+  // TODO set tighter access control
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+    HttpServletResponse response = (HttpServletResponse) res;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+    try {
+      chain.doFilter(req, res);
+    } catch (IOException | ServletException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) {}
+
+  @Override
+  public void destroy() {}
+
+}
